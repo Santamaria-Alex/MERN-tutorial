cfb45e7263f3872ba0d1a284c76a5a54d369e292
Alex Santamaria <santamariaalex146@gmail.com>
2022-03-08 15:59:08 -0600

    initialized express

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+//express.js, a node.js framework
+const express = require("express");
+//to have environment variables
+//config allows us to have a dotenv file with variables in it
+const dotenv = require("dotenv").config();
+//port we want our server to run on
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
