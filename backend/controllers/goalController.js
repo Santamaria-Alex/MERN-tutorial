4fbc1a94f417ab715b1fd826cdfe8c94f557d4c5
Alex Santamaria <santamariaalex146@gmail.com>
2022-03-08 16:51:54 -0600

    controllers, routes and async handler setup

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,30 +1,38 @@
+//have to download async handler npm install express-async-handler
+const asyncHandler = require("express-async-handler");
+
 //Get goals
 //@route GET /api/goals
 //@access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Get Goals" });
-};
+});
 
 //Set goals
 //@route POST /api/goals
 //@access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add a text field");
+  }
+
   res.status(200).json({ message: "Set Goal" });
-};
+});
 
 //Update goals
 //@route PUT /api/goals
 //@access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update goal ${req.params.id}` });
-};
+});
 
 //Delete goals
 //@route DELETE /api/goals
 //@access Private
-const deleteGoal = (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -6,10 +6,18 @@ const dotenv = require("dotenv").config();
 //port we want our server to run on
 const port = process.env.PORT || 5000;
 
+//error handler
+const { errorHandler } = require("./middleware/errorMiddleware");
+
 const app = express();
+
+//middleware??
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(errorHandler);
+
+app.use("/api/goals", require("./routes/goalRoutes"));
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
 });
-
-app.use("/api/goals", require("./routes/goalRoutes"));
